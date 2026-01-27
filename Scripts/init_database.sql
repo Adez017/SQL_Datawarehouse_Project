acc9ec2024346ec93cfe18678c6890cbcf6241bd
aditya singh rathore <2203031050017@paruluniversity.ac.in>
2026-01-27 14:08:49 +0530

    Create init_database.sql

---
+++ Scripts/init_database.sql
@@ -0,0 +1,27 @@
+=========================================
+create Databases and Schemas 
+=========================================
+Script Purposes:
+	This Script create a new database named 'DATAWAREHOUSE' .
+	Additionally , the scripts sets up three schemas within the database : 'silver','bronze' and 'gold'.
+
+
+-- creating  the database 'DATAWAREHOUSE'
+CREATE DATABASE DATAWAREHOUSE
+GO
+
+
+USE DATAWAREHOUSE;
+
+-- creating the schema for the three layers i.e. bronze , silver , gold
+
+create schema bronze;
+
+go
+create schema silver;
+
+go
+
+create schema gold;
+
+go
